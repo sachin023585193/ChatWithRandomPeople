f033972ada0815f04cef19f800d37207bcf983a7
sachin023585193 <sachin023585193@gmail.com>
2022-03-21 18:13:13 +0545

    .

--- chatWithRandomPeople/asgi.py
+++ chatWithRandomPeople/asgi.py
@@ -23,8 +23,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatWithRandomPeople.settings")
-# django_asgi_app = get_asgi_application()
-django.setup()
+django_asgi_app = get_asgi_application()
+# django.setup()
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
